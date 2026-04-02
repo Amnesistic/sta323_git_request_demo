d00612a4d75fb3e4e27e2a854582f4b6a8395641
YU Luodan <12310927@mail.sustech.edu.cn>
2026-04-02 17:34:59 +0800

    Delete check_dir.py

--- check_dir.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-def main():
-    if os.getcwd() != os.path.dirname(os.path.abspath(__file__)):
-        print("Please run this script from its own directory.")
-        sys.exit(1)
